b58ba51ae63767ec6602e70fd49397a9463ae1ac
Betterteng <betterteng@gmail.com>
2017-11-06 10:47:57 +1100

    Done

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var randomNum1 : Int = 0
+    var randomNum2 : Int = 0
+    
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomNum1 = Int(arc4random_uniform(6)) + 1
+        randomNum2 = Int(arc4random_uniform(6)) + 1
+        
+        // This method can also achieve the goal...
+        diceImage1.image = UIImage(named: "dice\(randomNum1)")
+        diceImage2.image = UIImage(named: "dice\(randomNum2)")
+        
+    }
+    
+    
+    
 }
-
